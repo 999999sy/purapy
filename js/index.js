c85b7d31296fa3d261bf947ea75e4d3bd2c745b1
999999sy <33650909+999999sy@users.noreply.github.com>
2018-03-04 19:45:59 +0900

    d

--- js/index.js
+++ js/index.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
 
+  // concept slide start
 
+  var active_slide = 0,
+      dom_slides = $('.concept ul li img'),
+      num_slides = dom_slides.length;
+    console.log(num_slides);
+    //  slide start
+  var start = setInterval(function() {
+
+    // 슬라이드
+    ++active_slide;
+    dom_slides.removeClass('active').eq(active_slide).addClass('active');
+    if(active_slide==4){
+      active_slide=1;
+        dom_slides.removeClass('active').eq(active_slide).addClass('active');
+    }
+
+})
 
 })
