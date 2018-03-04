b135e916ec9c8ec12df25e0317dfaf0db536546d
999999sy <33650909+999999sy@users.noreply.github.com>
2018-03-05 01:18:12 +0900

    일단 완성

--- js/index.js
+++ js/index.js
@@ -1,22 +1,23 @@
 $(document).ready(function(){
 
-  // concept slide start
-
-  var active_slide = 0,
-      dom_slides = $('.concept ul li img'),
-      num_slides = dom_slides.length;
-    console.log(num_slides);
-    //  slide start
-  var start = setInterval(function() {
-
-    // 슬라이드
-    ++active_slide;
-    dom_slides.removeClass('active').eq(active_slide).addClass('active');
-    if(active_slide==4){
-      active_slide=1;
-        dom_slides.removeClass('active').eq(active_slide).addClass('active');
-    }
-
+$('.top').click(function(){
+  $('html,body').animate({scrollTop:0},1500);
 })
 
+  // concept slide start
+
+var active_slide = 0,
+    dom_slides = $('.concept ul li'),
+    num_slides = dom_slides.length;
+
+    $('.concept ul li:first-child').addClass('active');
+        var start = setInterval(function(){
+          ++active_slide;
+            dom_slides.removeClass('active').eq(active_slide).addClass('active');
+            console.log(active_slide);
+          if(active_slide==4){
+            active_slide=1;
+              dom_slides.removeClass('active').eq(active_slide).addClass('active');
+          }
+        },3000)
 })
